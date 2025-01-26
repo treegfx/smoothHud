3d63bca5a961e158f602406de1fccf21ec599644
Withors <162058124+Withors@users.noreply.github.com>
2025-01-26 17:19:37 +0200

    w

--- forge/src/main/java/dev/smoothhud/Mod.java
+++ forge/src/main/java/dev/smoothhud/Mod.java
@@ -1,5 +1,18 @@
 package dev.smoothhud;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+import java.util.Optional;
+import java.util.function.BiFunction;
+
 @net.minecraftforge.fml.common.Mod("smoothhud")
 public class Mod {
     public Mod() {
--- forge/src/main/java/dev/smoothhud/mixin/InGameHudMixin.java
+++
@@ -1,4 +0,0 @@
-package dev.smoothhud.mixin;
-
-public class InGameHudMixin {
-}
